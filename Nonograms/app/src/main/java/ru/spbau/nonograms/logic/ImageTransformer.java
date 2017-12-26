5825ba468fe9c40d10fb07bd328e9c21b36f1df1
Anton Yermilov <anton.yermilov@gmail.com>
2017-12-26 03:56:40 +0300

    updated logic implementation, waiting for server connection

--- Nonograms/app/src/main/java/ru/spbau/nonograms/logic/ImageTransformer.java
+++ Nonograms/app/src/main/java/ru/spbau/nonograms/logic/ImageTransformer.java
@@ -1,7 +1,6 @@
 package ru.spbau.nonograms.logic;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.Matrix;
 
 import java.util.Arrays;
@@ -11,36 +10,6 @@ import java.util.Arrays;
  * Temporarily supports transforming to black'n'white image only.
  */
 class ImageTransformer {
-    static final int MIN_GRAY_SCALE_TYPE = 0;
-    static final int MAX_GRAY_SCALE_TYPE = 2;
-
-    /**
-     * Grayscales image and compresses it by selecting main colors in it.
-     * Returns the color of the background in RGB format.
-     *
-     * @param image      specified image
-     * @param mainColors number of main colors
-     *
-     * @return the color of the background in RGB format
-     */
-    static int selectMainColors(Bitmap image, int mainColors, int grayScaleType) {
-        int backgroundColor = 0;
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
-                int color = image.getPixel(x, y);
-                int r = Color.red(color);
-                int g = Color.green(color);
-                int b = Color.blue(color);
-
-                int grayTone = grayScale(r, g, b, grayScaleType);
-                grayTone = getOptimalColor(grayTone, mainColors);
-
-                backgroundColor = Math.max(backgroundColor, grayTone);
-                image.setPixel(x, y, getARGB(255, grayTone, grayTone, grayTone));
-            }
-        }
-        return getARGB(255, backgroundColor, backgroundColor, backgroundColor);
-    }
 
     /**
      * Creates new image with the size, multiplied in both directions by the specified value.
@@ -49,12 +18,9 @@ class ImageTransformer {
      * @param zoomWidth      width multiplier
      * @param zoomHeight     height multiplier
      * @param removeOriginal if {@code true}, deletes the specified image
-     * @param mutable        if {@code true}, makes the new image mutable
-     *
      * @return new image with decreased size
      */
-    static Bitmap increaseSize(Bitmap image, int zoomWidth, int zoomHeight,
-                               boolean removeOriginal, boolean mutable) {
+    static Bitmap increaseSize(Bitmap image, int zoomWidth, int zoomHeight, boolean removeOriginal) {
         int width = image.getWidth();
         int height = image.getHeight();
         int newWidth = width * zoomWidth;
@@ -75,9 +41,6 @@ class ImageTransformer {
         if (removeOriginal) {
             image.recycle();
         }
-        if (mutable) {
-            return newImage.copy(newImage.getConfig(), true);
-        }
         return newImage;
     }
 
@@ -88,12 +51,9 @@ class ImageTransformer {
      * @param newWidth       width if the new image
      * @param newHeight      height of the new image
      * @param removeOriginal if {@code true}, deletes the specified image
-     * @param mutable        if {@code true}, makes the new image mutable
-     *
      * @return new image with decreased size
      */
-    static Bitmap decreaseSize(Bitmap image, int newWidth, int newHeight,
-                               boolean removeOriginal, boolean mutable) {
+    static Bitmap decreaseSize(Bitmap image, int newWidth, int newHeight, boolean removeOriginal) {
         float scaleWidth = ((float) newWidth) / image.getWidth();
         float scaleHeight = ((float) newHeight) / image.getHeight();
 
@@ -105,62 +65,7 @@ class ImageTransformer {
         if (removeOriginal) {
             image.recycle();
         }
-        if (mutable) {
-            return newImage.copy(newImage.getConfig(), true);
-        }
         return newImage;
-    }
-
-    /**
-     * Returns gray tone that corresponds to the specified color.
-     *
-     * @param r    red component of color
-     * @param g    green component of color
-     * @param b    blue component of color
-     * @param type type of grayscale
-     *
-     * @return gray tone that corresponds to the specified color
-     */
-    private static int grayScale(int r, int g, int b, int type) {
-        if (type == 1) {
-            return Math.min(255, (int) (0.2162 * r + 0.7152 * g + 0.0722 * b));
-        }
-        if (type == 2) {
-            return Math.min(255, (int) (0.299 * r + 0.587 * g + 0.114 * b));
-        }
-        return Math.max(r, Math.max(g, b));
-    }
-
-    /**
-     * Selects optimal color from the number of main ones.
-     *
-     * @param color      specified color
-     * @param mainColors number of main colors
-     *
-     * @return optimal color
-     */
-    private static int getOptimalColor(int color, int mainColors) {
-        int nearest = 0;
-        for (int i = 0; i < mainColors; i++) {
-            if (255 * i / mainColors <= color) {
-                nearest = i;
-            }
-        }
-        return Math.min(255, (256 + 255 / (mainColors - 1)) * nearest / mainColors);
-    }
-
-    /**
-     * Returns color in argb type.
-     *
-     * @param a alpha component of color
-     * @param r red component of color
-     * @param g green component of color
-     * @param b blue component of color
-     *
-     * @return color in argb type
-     */
-    private static int getARGB(int a, int r, int g, int b) {
-        return ((a & 255) << 24) | ((r & 255) << 16) | ((g & 255) << 8) | (b & 255);
     }
 
 }
--- Nonograms/app/src/main/java/ru/spbau/nonograms/logic/NonogramLogic.java
+++ Nonograms/app/src/main/java/ru/spbau/nonograms/logic/NonogramLogic.java
@@ -1,16 +1,19 @@
 package ru.spbau.nonograms.logic;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import ru.spbau.nonograms.local_database.CurrentCrosswordState;
+import ru.spbau.nonograms.model.Image;
 
 /**
  * Supports creating and checking nonograms.
  */
 public class NonogramLogic {
-    private static final int DIMENSION_MAX_SIZE = 1000;
-    private static NonogramImage lastNonogram = null;
+    private static final int DIMENSION_MAX_SIZE = 720;
+    private static CurrentCrosswordState lastNonogram = null;
 
     /**
      * Creates nonogram with specified size and number of colors from specified image and
@@ -29,37 +32,31 @@ public class NonogramLogic {
             return null;
         }
 
-        int previousWidth = image.getWidth();
         int previousHeight = image.getHeight();
+        int previousWidth = image.getWidth();
 
-        float scale = Math.max((float) previousWidth, (float) previousHeight) / DIMENSION_MAX_SIZE;
+        float scale = Math.max((float) previousHeight, (float) previousWidth) / DIMENSION_MAX_SIZE;
         if (scale > 1.0) {
-            previousWidth = (int) (previousWidth / scale);
             previousHeight = (int) (previousHeight / scale);
+            previousWidth = (int) (previousWidth / scale);
         }
 
-        System.err.println(image.getWidth() + " " + image.getHeight());
+        Logger.getGlobal().logp(Level.INFO, "NonogramLogic", "createNonogram", "Decreasing size...");
+        image = ImageTransformer.decreaseSize(image, width, height, false);
 
-        System.err.println("Decreasing size");
-        image = ImageTransformer.decreaseSize(image, width, height, false, true);
-        System.err.println("Selecting colors");
-        int backgroundColor = ImageTransformer.selectMainColors(image, colors, 2);
-        System.err.println("Creating nonogram");
-        lastNonogram = NonogramCreator.createNonogram(image, backgroundColor);
-        System.err.println("Solving...");
-        if (!canSolve(lastNonogram)) {
-            lastNonogram = null;
-        }
-        System.err.println("Done");
+        Logger.getGlobal().logp(Level.INFO, "NonogramLogic", "createNonogram", "Creating nonogram...");
+        Image nonogram = new Image(image, colors); //TODO send to server
+
+        Logger.getGlobal().logp(Level.INFO, "NonogramImage", "createNonogram", "Increasing size...");
         return ImageTransformer.increaseSize(image,
-                previousWidth / width, previousHeight / height, true, false);
+                previousWidth / width, previousHeight / height, true);
     }
 
     /**
      * Returns last created nonogram if it was created successfully or null otherwise.
      * @return last created nonogram
      */
-    public static NonogramImage getLastNonogram() {
+    public static CurrentCrosswordState getLastNonogram() {
         return lastNonogram;
     }
 
@@ -77,49 +74,13 @@ public class NonogramLogic {
      * @param image specified nonogram image
      * @return {@code true} if nonogram can be solved; {@code false} otherwise
      */
-    private static boolean canSolve(NonogramImage image) {
-        boolean result = new MulticolorNonogramSolver(image).solve();
-        System.err.println("Has solution: " + result);
+    private static boolean canSolve(CurrentCrosswordState image) {
+        Logger.getGlobal().logp(Level.INFO, "NonogramLogic", "canSolve",
+                "Checking nonogram for solvability...");
+        boolean result = true; //TODO send to server
+        Logger.getGlobal().logp(Level.INFO, "NonogramLogic", "canSolve",
+                "Has solution: " + result);
         return result;
-    }
-
-    public static void main(String[] args) {
-        NonogramImage image = new NonogramImage(new int[][]{{0, 1, 0, 1, 0},
-                {1, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 0}}, 0);
-
-        CurrentCrosswordState.ColoredValue[] black = new CurrentCrosswordState.ColoredValue[]{
-                new CurrentCrosswordState.ColoredValue(1, Color.BLACK),
-                new CurrentCrosswordState.ColoredValue(2, Color.BLACK)};
-
-        CurrentCrosswordState.ColoredValue b = new CurrentCrosswordState.ColoredValue(
-                CurrentCrosswordState.FILLED_CELL, Color.BLACK);
-        CurrentCrosswordState.ColoredValue w = new CurrentCrosswordState.ColoredValue(0, Color.WHITE);
-
-        CurrentCrosswordState.ColoredValue[][] field = new CurrentCrosswordState.ColoredValue[][]{
-                {w, b, b, w, w},
-                {b, w, w, b, w},
-                {w, b, w, w, b},
-                {b, w, w, b, w},
-                {w, b, b, w, w}
-        };
-        CurrentCrosswordState.ColoredValue[][] rows = new CurrentCrosswordState.ColoredValue[][]{
-                {black[0], black[0]},
-                {black[0], black[0], black[0]},
-                {black[0], black[0]},
-                {black[0], black[0]},
-                {black[0]}
-        };
-        CurrentCrosswordState.ColoredValue[][] columns = new CurrentCrosswordState.ColoredValue[][]{
-                {black[1]},
-                {black[0], black[0]},
-                {black[0], black[0]},
-                {black[0], black[0]},
-                {black[1]}
-        };
-
-        CurrentCrosswordState state = new CurrentCrosswordState(rows, columns,
-                new int[]{Color.BLACK}, field);
-        System.err.println(NonogramChecker.check(state));
     }
 
 }
